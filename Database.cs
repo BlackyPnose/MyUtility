4edc97111801a37c8bb7fc6a508dccae210832c5
アレクサ <https://github.com/AlexaDotChan/Attempt.git>
2021-12-07 02:42:24 +0900

    Add project files.

---
+++ Database.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//I install the NuGet Package for MySql. Then use it
+using MySql.Data.MySqlClient;
+
+
+namespace MyUtility
+{
+    class Database
+    {
+        //I set and rename my connection with the database
+        private MySqlConnection con;
+
+        //I set all the needed information to access my server
+        public Database(string nameDB, string server = "localhost", string user = "root", string pass = "rooot")
+        {
+            con = new MySqlConnection($"SERVER={server};DATABASE={nameDB};UID={user};Password={pass};");
+        }
+
+        public List<Dictionary<string, string>> Read(string query)
+        {
+            List<Dictionary<string, string>> ris = new List<Dictionary<string, string>>();
+
+            con.Open();
+
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Dictionary<string, string> line = new Dictionary<string, string>();
+
+                for (int i = 0; i < dr.FieldCount; i++)
+                    line.Add(dr.GetName(i).ToLower(), dr.GetValue(i).ToString());
+
+                ris.Add(line);
+            }
+
+            dr.Close();
+            con.Close();
+
+            return ris;
+        }
+
+        public bool Update(string query)
+        {
+            try
+            {
+                con.Open();
+
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Error during the update() from query:\n{query.ToUpper()}");
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public Dictionary<string, string> ReadOne(string query)
+        {
+            try
+            {
+                return Read(query)[0];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Entity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Reflection;
+
+namespace MyUtility
+{
+    //I need to set this class public and abstract to every other class can access and inherit
+    public abstract class Entity
+    {
+        //Properties
+        public int Id { get; set; }
+
+        //Constructor
+        public Entity() { }
+
+        public Entity(int id)
+        {
+            Id = id;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            string ris = "";
+
+            foreach (PropertyInfo property in this.GetType().GetProperties())
+                ris += $"{property.Name}: {property.GetValue(this)}\n";
+
+            return ris;
+        }
+        public void FromDictionary(Dictionary<string, string> riga)
+        {
+
+            foreach (PropertyInfo property in this.GetType().GetProperties())
+            {
+                if (riga.ContainsKey(property.Name.ToLower()))
+                {
+                    object valore = riga[property.Name.ToLower()];
+
+                    switch (property.PropertyType.Name.ToLower())
+                    {
+                        case "datetime":
+                            valore = DateTime.Parse(riga[property.Name.ToLower()]);
+                            break;
+                        case "int32":
+                            valore = int.Parse(riga[property.Name.ToLower()]);
+                            break;
+                        case "double":
+                            valore = double.Parse(riga[property.Name.ToLower()]);
+                            break;
+                        case "boolean":
+                            valore = bool.Parse(riga[property.Name.ToLower()]);
+                            break;
+                    }
+                    property.SetValue(this, valore);
+                }
+            }
+
+
+        }
+    }
+}
